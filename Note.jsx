a1217ad881f12d7eb85dc3f31cc57ee4a9093870
ALINRAJR <85385547+ALINRAJR@users.noreply.github.com>
2021-06-06 15:47:22 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Javascript and React.jsx</h1>
+      <p>
+        this is an amazing boot camp, this bootcamp is conducted by shaurya
+        siinha. it was really an mazing experience to me to look forward for the
+        new scopes.and i really thank shaurya for taking such an effort to hep
+        us even during this pandamic
+      </p>
+    </div>
+  );
+}
+
+export default Note;
